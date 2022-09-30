9b3b3d75cffd11a0084e27d3888c621597511c1a
darshan-k3 <96890748+darshan-k3@users.noreply.github.com>
2022-09-30 13:45:00 +0100

    frequency
    attempt 1

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,14 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
+    #e.g. items = [a,b,a,a,a,a,b,a]
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if(frequencies.get(item) == None):
+            frequencies[item] = 1
+        else:
+            frequencies[item] = frequencies.get(item) + 1
+
     return frequencies
